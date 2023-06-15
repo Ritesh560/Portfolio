f297559dd764de09d76ab410ad56d3031362b9eb
Ritesh Kumar <singlaritesh007@gmail.com>
2023-06-15 18:57:49 +0530

    feat: Responsive Home

--- src/compoents/Home/Home.jsx
+++ src/compoents/Home/Home.jsx
@@ -3,14 +3,14 @@ import styles from "./Home.module.scss"
 
 const Home = ({ theme }) => {
   return (
-    <div className="h-[100vh] pd-14 flex justify-between items-center flex-col md:flex-row" style={{ color: theme.text }}>
+    <div className="h-[100vh] pt-8 flex  items-center gap-16 flex-col md:flex-row md:justify-between md:pt-0 " style={{ color: theme.text }}>
       <div className="w-full md:w-2/5 flex items-center justify-center">
         <AvatarDark className="aspect-w-531 aspect-h-521 " />
       </div>
       <div className="w-full md:w-3/5 flex flex-col gap-8">
-        <div className={`${styles.title} ${styles.darkGradient}`}>Turning Vision Into Reality With Code.</div>
-        <div className="text-2xl flex items-center justify-center px-2">Skilled full-stack developer specializing in React.js and web development, dedicated to translating ideas into impressive web applications.</div>
-        <div className="flex items-center gap-20 pl-2 mt-10">
+        <div className={`${styles.title} ${styles.darkGradient} text-center text-3xl  md:text-3xl md:text-left md:leading-relaxed lg:text-3xl lg:leading-relaxed xl:text-6xl xl:leading-normal`}>Turning Vision Into Reality With Code.</div>
+        <div className="text-2xl flex items-center text-center  px-2 md:text-xl md:text-left xl:text-2xl">Skilled full-stack developer specializing in React.js and web development, dedicated to translating ideas into impressive web applications.</div>
+        <div className="flex items-center gap-20 pl-2 mt-10 justify-center md:justify-start">
           <div className={`flex items-center justify-center gap-[5px] ${styles.button}`}>
             Resume <NewTabIcon />
           </div>
--- src/compoents/Skills/Skills.jsx
+++ src/compoents/Skills/Skills.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react"
 
 function Skills() {
-  return <div className="h-[100vh] bg-yellow-100">home</div>
-  // return <></>
+  // return (
+  //   <div className="h-[100vh] " style={{ border: "2px solid white" }}>
+  //     home
+  //   </div>
+  // )
+  return <></>
 }
 
 export default Skills
