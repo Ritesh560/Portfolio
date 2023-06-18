f9cfbf6bde32137ba2ff9b217bd038fafe1caede
Ritesh Kumar <singlaritesh007@gmail.com>
2023-06-18 18:39:32 +0530

    feat: complete look

--- src/compoents/Footer/Footer.jsx
+++ src/compoents/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import { FacebookLogoOutline, GithubLogoOutline, InstagramLogoOutline, LinkdinLogoOutline, TwitterLogoOutline } from "../../libs/icons/icon"
+import { urls } from "../constants"
 
 const Footer = () => {
   return (
@@ -6,11 +7,21 @@ const Footer = () => {
       <p>Copyright @2023</p>
       <p>Ritesh Kumar</p>
       <div id="icons" className="flex items-center justify-between gap-4 ">
-        <LinkdinLogoOutline className="h-12" />
-        <GithubLogoOutline className="h-12" />
-        <TwitterLogoOutline className="h-12" />
-        <InstagramLogoOutline className="h-12" />
-        <FacebookLogoOutline className="h-12" />
+        <a href={urls?.linkdin} target="_blank" rel="noopener noreferrer">
+          <LinkdinLogoOutline className="h-12" />
+        </a>
+        <a href={urls?.github} target="_blank" rel="noopener noreferrer">
+          <GithubLogoOutline className="h-12" />
+        </a>
+        <a href={urls?.twitter} target="_blank" rel="noopener noreferrer">
+          <TwitterLogoOutline className="h-12" />
+        </a>
+        <a href={urls?.instagram} target="_blank" rel="noopener noreferrer">
+          <InstagramLogoOutline className="h-12" />
+        </a>
+        <a href={urls?.facebook} target="_blank" rel="noopener noreferrer">
+          <FacebookLogoOutline className="h-12" />
+        </a>
       </div>
     </div>
   )
